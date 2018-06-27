081ea957ce0e2d51543e7f4e9831e5b9b5e14bb2
Rossco <ross.weaver@calidadsolutions.onmicrosoft.com>
2018-06-27 11:48:50 +0000

    Add indicator functionality to car

--- chassis.js
+++ chassis.js
@@ -1,1 +1,2 @@
 // add wiring between console and headlights.
+// Add wiring between console and indicators.
--- exterior.js
+++ exterior.js
@@ -1,1 +1,2 @@
 // Add headlights to the fron to the car.
+// Add a right and left indicator to the front and back of the car.
